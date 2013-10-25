448ca1b212e5d5b68732327bc8c41b75bf29c1b6
Tad <tad0616@gmail.com>
2013-10-25 11:15:18 +0800

    格式更新

--- header.php
+++ header.php
@@ -1,10 +1,4 @@
 <?php
-//  ------------------------------------------------------------------------ //
-// 本模組由 tad 製作
-// 製作日期：2008-03-25
-// $Id: header.php,v 1.1 2008/04/04 07:10:34 tad Exp $
-// ------------------------------------------------------------------------- //
-
 include "../../mainfile.php";
 include "function.php";
 
@@ -16,7 +10,7 @@ if ($xoopsUser) {
 }
 
 if($isAdmin){
-    $interface_menu[_TO_ADMIN_PAGE]="admin/index.php";
+    $interface_menu[_TO_ADMIN_PAGE]="admin/main.php";
 }
 
 ?>
