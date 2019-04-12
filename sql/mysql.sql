04d2ee7e55afe42e499e189ec8b1b2773fb2d1c6
mambax7 <mambax7@gmail.com>
2019-04-12 03:39:43 -0400

    Case mismatch
    Signed-off-by: mambax7 <mambax7@gmail.com>

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -2,7 +2,7 @@ CREATE TABLE tad_cbox (
   `sn` mediumint(9) unsigned NOT NULL  auto_increment ,
   `publisher` varchar(255) NOT NULL default '' ,
   `msg` text NOT NULL ,
-  `post_date` datetime NOT NULL default '0000-00-00 00:00:00' ,
+  `post_date` datetime NOT NULL ,
   `ip` varchar(255) NOT NULL default '' ,
   `only_root` enum('0','1') NOT NULL default '0' ,
   `root_msg` text NOT NULL ,
